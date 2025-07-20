8d9800d2a4ed981160539109a1429ea279ba13ec
meethatkar <meethatkar000@gmail.com>
2025-07-20 18:00:24 +0530

    addev movie and replace <a> with button & button hover effect

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ tl.from("nav>h1",{
     x:"-100%",
     scale: 0.8,
     opacity:0,
-    duration:1.5,
-    ease:"back.out(0.5)"
+    duration:1,
+    ease:"back.out(1)"
 })
 
-tl.from(".L",{
-    x:"100%",
-    scale:0.9,
-    opacity:0,
-    duration:1,
-    ease:"back.out(0.5)",
-    // stagger: 0.5,
-})
+// tl.from(".L",{
+//     x:"100%",
+//     scale:0.9,
+//     opacity:0,
+//     duration:1,
+//     ease:"back.out(0.5)",
+//     // stagger: 0.5,
+// })
 
-tl.from(".R",{
-    x:"-100%",
-    scale:0.9,
-    opacity:0,
-    duration:1,
-    ease:"back.out(0.5)",
-    // stagger: 0.5,
-})
+// tl.from(".R",{
+//     x:"-100%",
+//     scale:0.9,
+//     opacity:0,
+//     duration:1,
+//     ease:"back.out(0.5)",
+//     // stagger: 0.5,
+// })
